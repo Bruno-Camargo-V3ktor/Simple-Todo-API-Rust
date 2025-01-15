c007822f213f9c830b1918b1013ab706f85130f4
Bruno C. <v3ktoryt@gmail.com>
2025-01-14 21:57:47 -0300

    feat: Endpoint to add a new task

--- src/models/task.rs
+++ src/models/task.rs
@@ -3,7 +3,7 @@ use rocket::serde::{Deserialize, Serialize};
 // Structs
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Task {
-    pub id: usize,
+    pub id: Option<usize>,
     pub title: String,
     pub description: String,
     pub completed: bool,
--- src/routes/tasks.rs
+++ src/routes/tasks.rs
@@ -1,7 +1,9 @@
 use crate::db::mem_db::Db;
 use crate::models::task::Task;
+use rocket::http::Status;
+use rocket::response::status;
 use rocket::serde::json::Json;
-use rocket::{Route, State, get, routes};
+use rocket::{Route, State, get, post, routes};
 
 // ENDPOINTS
 #[get("/")]
@@ -11,7 +13,23 @@ pub fn list_task(db: &State<Db>) -> Json<Vec<Task>> {
     Json(tasks)
 }
 
+#[post("/", format = "application/json", data = "<data>")]
+pub fn add_task(
+    db: &State<Db>,
+    data: Json<Task>,
+) -> Result<status::Custom<()>, status::BadRequest<&'static str>> {
+    let mut db = db.lock().expect("Falid load db");
+    let mut task = data.into_inner();
+
+    let new_id = db.keys().max().unwrap_or(&0) + 1;
+    task.id = Some(new_id);
+
+    db.insert(new_id, task);
+
+    Ok(status::Custom(Status::Created, ()))
+}
+
 // FUNCTIONS
 pub fn routes() -> Vec<Route> {
-    routes![list_task]
+    routes![list_task, add_task]
 }
