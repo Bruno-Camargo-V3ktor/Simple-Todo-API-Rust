5cf2f2a54ae092fa1e6c226d0a69dc8d0900f6bc
Bruno C. <v3ktoryt@gmail.com>
2025-01-15 23:11:33 -0300

    Creating base template file and task html file, along with your endpoint

--- src/views/views.rs
+++ src/views/views.rs
@@ -2,15 +2,12 @@ use rocket::{Route, get, routes};
 use rocket_dyn_templates::{Template, context};
 
 // ENDPOINTS
-#[get("/")]
-fn home_page() -> Template {
-    Template::render("index", context! {
-        title: "Todo List",
-        message: "Welcome!"
-    })
+#[get("/todo")]
+fn todo_page() -> Template {
+    Template::render("todo", context! {})
 }
 
 // FUNCTIONS
 pub fn routes() -> Vec<Route> {
-    routes![home_page]
+    routes![todo_page]
 }
